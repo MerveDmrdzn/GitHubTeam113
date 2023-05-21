034dc9e31d75ffe4bae6c8f1fec340cb85d8d2d4
MRV <mrvdmrdzn1@gmail.com>
2023-05-21 17:25:13 +0300

    IlkGıtHub

---
+++ src/Deneme_01.java
@@ -0,0 +1,12 @@
+public class Deneme_01 {
+    public static void main(String[] args) {
+        System.out.println("Deneme 1 ..2...");
+
+        /*git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MerveDmrdzn/GitHubTeam113.git
+git push -u origin main*/
+    }
+}
