0b957f9490fb5d9ee3492534da3a5adccdb9f939
MRV <mrvdmrdzn1@gmail.com>
2023-05-21 17:33:22 +0300

    2105

--- src/deneme_001.java
+++ src/deneme_001.java
@@ -1,5 +1,7 @@
 public class deneme_001 {
     public static void main(String[] args) {
         System.out.println("naber dünya ?");
+        System.out.println("-----------");
+        System.out.println("have a nıce day");
     }
 }
